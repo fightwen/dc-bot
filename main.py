dadd4e82b63962835f020145fbd842fc5d63e653
wen <bzdlac@gmail.com>
2017-09-02 18:35:46 +0800

    hello test

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('log in')
+	print(client.user.name)
+	print(client.id)
+
+@client.event
+async def on_message(message):
+	if(message.content == "Hello"):
+		await client.send_message(message.channel,"World")
+
+client.run("MzUxMjUzOTEwMDU4Njk2NzA0.DIP61Q.DVeU9oIP6a37n7xgdZO7zgJzW-k")
