af8d43a10984394eab41f684be428c520457b93e
wen <bzdlac@gmail.com>
2017-10-01 16:34:37 +0800

    make done module

---
+++ botcheck.py
@@ -0,0 +1,22 @@
+def is_hope_channel(message):
+	if message.channel.id == '357191670825091084':
+		return True
+	return False
+
+def is_all_color_roles(role_name):
+	if "*" in role_name:
+		return True
+	return False 
+
+def check_is_error_color_mentions(roles,mention):
+	is_error_mention = True
+	for role in roles:
+		if role.mention == mention:
+			is_error_mention =  False
+
+	return is_error_mention
+def check_contain_http_img(url):
+	if url.startswith('http'):
+		if 'jpg' in url or 'png' in url or 'gif' in url:
+			return True
+	return False
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 import asyncio
 import random
+import botcheck
+import json
 
 client = discord.Client()
 ishope = False
-role_names=["red","blue","green"]
+
 
 @client.event
 async def on_ready():
@@ -13,7 +15,15 @@ async def on_ready():
 	print(client.user.id)
 
 @client.event
+async def on_reaction_add(reaction, user):
+	print(user.name)
+	print(reaction)
+
+@client.event
 async def on_message(message):
+	print(message.content)
+	print(message.attachments)
+	print(message.reactions)
 	if(message.content == "Hello"):
 		await client.send_message(message.channel,"World")
 	if(message.content.startswith("i!color")):
@@ -23,7 +33,7 @@ async def on_message(message):
 		if(not choose_color):
 			print('You never choose color!!')
 			return
-		if(check_is_error_color_mentions(message.server.roles,choose_color)):
+		if(botcheck.check_is_error_color_mentions(message.server.roles,choose_color)):
 			print('You choose error color!!')
 			return
 
@@ -33,29 +43,57 @@ async def on_message(message):
 		for arole in message.author.roles:
 			name = arole.name
 			print(name)
-			if(is_all_color_roles(name)):
+			if(botcheck.is_all_color_roles(name)):
 				print(name)
 				await client.remove_roles(message.author, arole)
 		await client.add_roles(message.author, role)
 
 		role_str = ''
 		for role in message.server.roles:
-			if(is_all_color_roles(role.name)):
+			if(botcheck.is_all_color_roles(role.name)):
 				role_str+=" "+ role.mention
 		
 		await client.send_message(message.channel,
 			"You can choose "+role_str)
-	if(message.content.startswith('i!hope')): 
-		if not is_hope_channel(message):
+
+	# check after i!hope, anyone finished picture
+	if(message.content.startswith('i!done')): 
+		if not botcheck.is_hope_channel(message):
 			await client.send_message(message.channel,
 			"error channel!!!")
 			return
+		global ishope
+		if ishope == False:
+			await client.send_message(message.channel,
+			"no hope!!!!!!")
+			return
+		done_pic = message.content[len('i!done'):].strip()
+		# check attach pic and http pic
+		if not message.attachments and !botcheck.check_contain_http_img(done_pic):
+			print('empty pic!!')
+			return
+		
+		if(not botcheck.check_contain_http_img(message.attachments[0]['url'])):
+			print('upload error data!!')
+			return
+
+		print (message.attachments[0]['url'])
+
+	if(message.content.startswith('i!hope')): 
+		if not botcheck.is_hope_channel(message):
+			await client.send_message(message.channel,
+			"error channel!!!")
+			return
+
 		global ishope
 		if ishope == True:
 			await client.send_message(message.channel,
 			"is done!!!!!!")
 			return
-
+		hope_topic = message.content[len('i!hope'):].strip()
+		if(not hope_topic):
+			print('You never choose topic!!')
+			return
 		member_names = []
 		for member in client.get_all_members():
 			print(member)
@@ -74,26 +112,12 @@ async def on_message(message):
 		print(new_member)
 		print(new_member.status)
 		ishope = True
+
 		await client.send_message(message.channel,
-			"{} 許願說要 ".format(message.author.mention)+new_member.mention+" 畫 xxxx")
+			"{} 許願說要 ".format(message.author.mention)+new_member.mention+" 畫 "+hope_topic)
+		
+			
+		
 
-
-def is_hope_channel(message):
-	if message.channel.id == '357191670825091084':
-		return True
-	return False
-
-def is_all_color_roles(role_name):
-	if "*" in role_name:
-		return True
-	return False 
-
-def check_is_error_color_mentions(roles,mention):
-	is_error_mention = True
-	for role in roles:
-		if role.mention == mention:
-			is_error_mention =  False
-
-	return is_error_mention
 
 client.run("MzUxMjUzOTEwMDU4Njk2NzA0.DIP61Q.DVeU9oIP6a37n7xgdZO7zgJzW-k")
