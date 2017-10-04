fd7d78bf00de3e9e702dc216ff8696ca5af2b861
wen <bzdlac@gmail.com>
2017-10-04 15:37:29 +0800

    no test

--- main.py
+++ main.py
@@ -222,4 +222,4 @@ async def del_all_colors(client,message):
 # if test_mode:
 	# client.run("MzUxMjUzOTEwMDU4Njk2NzA0.DIP61Q.DVeU9oIP6a37n7xgdZO7zgJzW-k")
 # else:
-	client.run("MzY0MDA1MTE2NDM0NTc5NDU2.DLXirQ.BxTq4iQKodHq9Whxlsih1YPS9Ho")
+client.run("MzY0MDA1MTE2NDM0NTc5NDU2.DLXirQ.BxTq4iQKodHq9Whxlsih1YPS9Ho")
